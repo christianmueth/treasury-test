d148ab3c69f0ef7006bc13645121e78ba5c61031
Christian Mueth <christianelijahmueth@gmail.com>
2026-06-13 19:13:12 -0400

    Fix deployed icon asset paths

--- app/head.tsx
+++ app/head.tsx
@@ -1,14 +1,9 @@
 export default function Head() {
   return (
     <>
-  {/* Primary favicon */}
-  <link rel="icon" href="/logo.ico" />
-  <link rel="shortcut icon" href="/logo.ico" />
-
-  {/* Apple touch icon for iOS */}
-  <link rel="apple-touch-icon" sizes="180x180" href="/logo-v2.png" />
-
-      {/* Theme color (used by some browsers when showing pinned tabs) */}
+      <link rel="icon" href="/logo.ico" />
+      <link rel="shortcut icon" href="/logo.ico" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/logo.png" />
       <meta name="theme-color" content="#ffffff" />
     </>
   );
